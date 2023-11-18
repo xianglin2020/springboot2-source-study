d3ec0e4250bbedb004e0c4853080d172d460a137
xianglin <xianglin2020@outlook.com>
2023-11-18 17:40:26 +0800

    Banner 解析

--- src/main/java/store/xianglin/sb2/SpringBoot2Application.java
+++ src/main/java/store/xianglin/sb2/SpringBoot2Application.java
@@ -1,19 +1,19 @@
 package store.xianglin.sb2;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.Banner;
+import org.springframework.boot.ResourceBanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import store.xianglin.sb2.Initializer.SecondInitializer;
-import store.xianglin.sb2.listener.SecondListener;
+import org.springframework.core.io.ClassPathResource;
 
 @SpringBootApplication
 @MapperScan("store.xianglin.sb2.mapper")
 public class SpringBoot2Application {
     public static void main(String[] args) {
-//        SpringApplication.run(SpringBoot2Application.class, args);
         var springApplication = new SpringApplication(SpringBoot2Application.class);
-        springApplication.addInitializers(new SecondInitializer());
-        springApplication.addListeners(new SecondListener());
+        springApplication.setBanner(new ResourceBanner(new ClassPathResource("favorite.txt")));
+        springApplication.setBannerMode(Banner.Mode.CONSOLE);
         springApplication.run(args);
     }
 }
