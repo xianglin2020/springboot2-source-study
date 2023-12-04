85b5102acce69f43e70a09e81b95ec19bc832289
xianglin <xianglin2020@outlook.com>
2023-12-04 19:53:58 +0800

    Servlet 容器解析

--- src/main/java/store/xianglin/sb2/SpringBoot2Application.java
+++ src/main/java/store/xianglin/sb2/SpringBoot2Application.java
@@ -3,22 +3,13 @@ package store.xianglin.sb2;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.util.Assert;
-
-import java.util.Properties;
 
 @SpringBootApplication
 @MapperScan("store.xianglin.sb2.mapper")
 @PropertySource({"classpath:demo.properties"})
 public class SpringBoot2Application {
     public static void main(String[] args) {
-        var springApplication = new SpringApplication(SpringBoot2Application.class);
-        var properties = new Properties();
-        properties.setProperty("property", "property from DefaultProperties");
-        springApplication.setDefaultProperties(properties);
-        var context = springApplication.run(args);
-        Assert.notNull(context.getBean("componentSuperConfiguration"));
+        SpringApplication.run(SpringBoot2Application.class, args);
     }
 }
